28dd6db6ccf353d83f9a1c3d943cefbcd83e556b
vvbv <jeison.cardona@correounivalle.edu.co>
2019-03-31 17:58:04 -0500

    Integral secuencial

--- integral/secuencial.cpp
+++ integral/secuencial.cpp
@@ -8,10 +8,10 @@ using std::endl;
 
 int main( int argc, char *argv[] ){
 
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
+    int a = atoi(argv[1]); // Límite inferior
+    int b = atoi(argv[2]); // Límite superior
     // Función ∫(ln(x)x²)dx
-    int partitions = atoi(argv[3]);
+    int partitions = atoi(argv[3]); 
     double rectangle_base = (b-a)/(double)partitions;
     
     double area = 0.0;
@@ -20,7 +20,7 @@ int main( int argc, char *argv[] ){
         area += rectangle_base * (log( x_i ) * pow( x_i , 2.0));
     }
     
-    cout << area << endl;
+    cout << "Integral: secuencial( a=" << a << "; b=" << b <<"; n=" << partitions << "; ) => " <<  area << endl;
     
     return 0;
 }
