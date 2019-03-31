e0017e03fc659c52f0866a2b05ceb836b5c8609c
vvbv <jeison.cardona@correounivalle.edu.co>
2019-03-31 14:56:15 -0500

    k como argumento

--- monte_carlo/deploy.sh
+++ monte_carlo/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm secuencial > /dev/null 2>/dev/null
 g++ secuencial.cpp -o secuencial
-./secuencial
+time ./secuencial $1
 rm distribuida_paralela > /dev/null 2>/dev/null
 mpic++ distribuida_paralela.cpp -o distribuida_paralela
-./distribuida_paralela
+time mpirun -np 4 distribuida_paralela $1
