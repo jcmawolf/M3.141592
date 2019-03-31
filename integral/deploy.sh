b8ac7fe27d7813ef90357c339905b6c4df394bec
vvbv <jeison.cardona@correounivalle.edu.co>
2019-03-31 17:34:16 -0500

    .

---
+++ integral/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+rm build/secuencial > /dev/null 2>/dev/null
+g++ secuencial.cpp -o build/secuencial
+# time build/secuencial $1
+# rm build/distribuida_paralela > /dev/null 2>/dev/null
+# mpic++ distribuida_paralela.cpp -o build/distribuida_paralela
+# time mpirun -np 4 build/distribuida_paralela $1
+# rm build/distribuida_paralela_mod > /dev/null 2>/dev/null
+# mpic++ distribuida_paralela_mod.cpp -o build/distribuida_paralela_mod
+# time mpirun -np 4 build/distribuida_paralela_mod $1
---
+++ integral/secuencial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using std::cout;
+using std::endl;
+
+
+int main( int argc, char *argv[] ){
+
+    
+
+    return 0;
+}
