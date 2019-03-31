df08da8b7df8922d1ba5bdb646323acdb2468811
vvbv <jeison.cardona@correounivalle.edu.co>
2019-03-31 13:56:31 -0500

    Corrección de mensajes

--- monte_carlo/deploy.sh
+++ monte_carlo/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-rm secuencial
+rm secuencial > /dev/null 2>/dev/null
 g++ secuencial.cpp -o secuencial
 ./secuencial
+rm distribuida_paralela > /dev/null 2>/dev/null
+mpic++ distribuida_paralela.cpp -o distribuida_paralela
+./distribuida_paralela
