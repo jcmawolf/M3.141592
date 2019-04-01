a4a5e36fb8ab2283e33ed587ccf53e9daeafe1a2
vvbv <jeison.cardona@correounivalle.edu.co>
2019-03-31 20:21:12 -0500

    Integral

--- integral/deploy.sh
+++ integral/deploy.sh
@@ -6,6 +6,6 @@ time build/secuencial $1 $2 $3
 rm build/distribuida_paralela > /dev/null 2>/dev/null
 mpic++ distribuida_paralela.cpp -o build/distribuida_paralela
 time mpirun -np 4 build/distribuida_paralela $1 $2 $3
-# rm build/distribuida_paralela_mod > /dev/null 2>/dev/null
-# mpic++ distribuida_paralela_mod.cpp -o build/distribuida_paralela_mod
-# time mpirun -np 4 build/distribuida_paralela_mod $1
+rm build/distribuida_paralela_mod > /dev/null 2>/dev/null
+mpic++ distribuida_paralela_mod.cpp -o build/distribuida_paralela_mod
+time mpirun -np 4 build/distribuida_paralela_mod $1 $2 $3
--- integral/distribuida_paralela_mod.cpp
+++ integral/distribuida_paralela_mod.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
-#include <ctime>
-#include <stdlib.h> 
-#include <climits>
-#include <mpi.h> 
 #include <cstdlib>
+#include <cmath> 
+#include <mpi.h> 
 
 using std::cout;
 using std::endl;
-
-double randNumber(int seed){
-    srand(time(NULL) + seed);
-    return (double) rand() / ( (double) RAND_MAX + 1);
-}
 
 int main( int argc, char *argv[] ){
 
@@ -23,58 +16,56 @@ int main( int argc, char *argv[] ){
 	int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    int circle_points = 0;
-    int square_points = 0;
-
-    double x, y;
-    int seed = time(NULL); //Cantidad de segundos desde 1970-01-01
-    int num_iterations_by_node = -1;
-    int num_iterations = -1;
-    double pi = 0;
+    int a; // Límite inferior
+    int b; // Límite superior
+    // Función ∫(ln(x)x²)dx
+    int partitions; 
+    double rectangle_base;
+    int num_iterations_by_node;
+        
+    double data_to_send[5];
     if( world_rank == 0 ){
-        num_iterations = atoi(argv[1]);
-        num_iterations_by_node = num_iterations / (world_size - 1);
+        a = atoi(argv[1]); 
+        data_to_send[0] = (double) a; // Límite inferior
+
+        b = atoi(argv[2]); 
+        data_to_send[1] = (double) b; // Límite superior
+
+        partitions = atoi(argv[3]); 
+        data_to_send[2] = (double) partitions;
+
+        num_iterations_by_node = partitions / (world_size - 1); 
+        data_to_send[3] = (double) num_iterations_by_node;
+
+        rectangle_base = (b-a)/(double)partitions; 
+        data_to_send[4] = rectangle_base;
     }
 
     // Envio
-    MPI_Bcast(&num_iterations_by_node, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
+    MPI_Bcast(&data_to_send, 5, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    double area = 0.0;
     if( world_rank != 0 ){
-        /** 
-         * Se evita que la primer semilla sea igual para ambos
-         * puntos. Se espera que el número de iteraciones sea
-         * menor al valor máximo de un entero.
-         **/
-        circle_points = 0;
-        square_points = 0;
-        x = randNumber(seed * INT_MAX + world_rank); //Desborde de variable intencional
-        y = randNumber(seed * -INT_MAX  + world_rank); //Desborde de variable intencional
-        for (int k = 1; k <= num_iterations_by_node; k++) {
-            double d = (x*x + y*y);
-            if( d <= 1.0 ){
-                circle_points++;
-            }
-            square_points++;
-            seed = time(NULL) * d; // Calculo de nueva semilla, multiplicada por d para adicionar algo de sal. 
-            x = randNumber(seed - k + world_rank); // Desechados en la última iteración, permiten el ahorro de la 
-            y = randNumber(seed + k + world_rank); // comprobación por iteración de si k == 0
+        
+        double partida =  data_to_send[0] + (((data_to_send[3] * world_rank) - data_to_send[3]) * data_to_send[4]);
+        for(int i = 0; i < data_to_send[3]; i++){
+            //cout << data_to_send[0] << " " << data_to_send[1] << " " << data_to_send[2] << " " << data_to_send[3] << " " << data_to_send[4] << endl;
+            double x_i =  (partida + (i * data_to_send[4]));
+            area += data_to_send[4] * (log( x_i ) * pow( x_i , 2.0));
         }
     }
 
-    int c_p[world_size];
-    int s_p[world_size];
-    MPI_Gather(&circle_points, 1, MPI_INT, c_p, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Gather(&square_points, 1, MPI_INT, s_p, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    double a_p[world_size];
+    MPI_Gather(&area, 1, MPI_DOUBLE, a_p, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     
     if( world_rank == 0 ){
-
+        
         for( int i = 0; i <= (world_size - 1); i++ ){
-            circle_points += c_p[ i ];
-            square_points += s_p[ i ];
+            area += a_p[ i ];
         }
 
-        pi =  4.0 * ( circle_points / (double) square_points );
-        cout << "Pi: Monte Carlo, distribuida/paralela mod( k=" << num_iterations << " => " << num_iterations_by_node << " ) => " <<  pi << endl;
+        cout << "Integral: distribuida/paralela mod( a=" << a << "; b=" << b <<"; n=" << partitions << " => " << num_iterations_by_node * (world_size - 1) << "; ) => " <<  area << endl;
+
     }
     MPI_Finalize();
 
