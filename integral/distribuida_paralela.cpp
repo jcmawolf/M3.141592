6b73e77a6cbca63ee5a53f137de30c34fc571fb5
vvbv <jeison.cardona@correounivalle.edu.co>
2019-03-31 19:26:43 -0500

    + distribución paralela

--- integral/distribuida_paralela.cpp
+++ integral/distribuida_paralela.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
-#include <ctime>
-#include <stdlib.h> 
-#include <climits>
-#include <mpi.h> 
 #include <cstdlib>
+#include <cmath> 
+#include <mpi.h> 
 
 using std::cout;
 using std::endl;
-
-double randNumber(int seed){
-    srand(time(NULL) + seed);
-    return (double) rand() / ( (double) RAND_MAX + 1);
-}
 
 int main( int argc, char *argv[] ){
 
@@ -23,64 +16,51 @@ int main( int argc, char *argv[] ){
 	int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    int circle_points = 0;
-    int square_points = 0;
-
-    double x, y;
-    int seed = time(NULL); //Cantidad de segundos desde 1970-01-01
-
     if( world_rank == 0 ){
-        double pi = -1.0;
-        int num_iterations = atoi(argv[1]);
-        int num_iterations_by_node = num_iterations / (world_size - 1);
-        int last_node_iterations = num_iterations_by_node + ( num_iterations - ( num_iterations_by_node * (world_size - 1 ) ) );
+        
+        int a = atoi(argv[1]); // Límite inferior
+        int b = atoi(argv[2]); // Límite superior
+        // Función ∫(ln(x)x²)dx
+        int partitions = atoi(argv[3]); 
+        double rectangle_base = (b-a)/(double)partitions;
+
+        int num_iterations_by_node = partitions / (world_size - 1);
+        int last_node_iterations = num_iterations_by_node + ( partitions - ( num_iterations_by_node * (world_size - 1 ) ) );
         
         //Envio
         for( int i = 1; i < world_size; i++ ){
+            double data_to_send[3];
+            data_to_send[0] =  a + (((num_iterations_by_node * i) - num_iterations_by_node) * rectangle_base); // Partida
+            data_to_send[2] = rectangle_base;
             if( i != ( world_size - 1 ) ){
-                MPI_Send(&num_iterations_by_node, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+                data_to_send[1] = num_iterations_by_node;
+                MPI_Send(&data_to_send, 3, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
             }else{
-                MPI_Send(&last_node_iterations, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+                data_to_send[1] = last_node_iterations;
+                MPI_Send(&data_to_send, 3, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
             }
         }
 
         //Recepción
+        double total_area = 0;
         for( int i = 1; i < world_size; i++ ){
-            int vals[2];
-            MPI_Recv(&vals, 2, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            circle_points += vals[0];
-            square_points += vals[1];
+            double p_area;
+            MPI_Recv(&p_area, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            total_area += p_area;
         }
-        
-        pi =  4.0 * ( circle_points / (double) square_points );
 
-        cout << "Pi: Monte Carlo, distribuida/paralela( k=" << num_iterations << " ) => " <<  pi << endl;
+        cout << "Integral: distribuida/paralela( a=" << a << "; b=" << b <<"; n=" << partitions << "; ) => " <<  total_area << endl;
 
     }else{
-        int num_node_iterations = 0;
-        MPI_Recv(&num_node_iterations, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        /** 
-         * Se evita que la primer semilla sea igual para ambos
-         * puntos. Se espera que el número de iteraciones sea
-         * menor al valor máximo de un entero.
-         **/
-        x = randNumber(seed * INT_MAX); //Desborde de los enteros
-        y = randNumber(seed * -INT_MAX); //Desborde de los enteros
-        for (int k = 1; k <= num_node_iterations; k++) {
-            double d = (x*x + y*y);
-            if( d <= 1.0 ){
-                circle_points++;
-            }
-            square_points++;
-            seed = time(NULL) * d; // Calculo de nueva semilla, multiplicada por d para adicionar algo de sal. 
-            x = randNumber(seed - k); // Desechados en la última iteración, permiten el ahorro de la 
-            y = randNumber(seed + k); // comprobación por iteración de si k == 0
+        double data[3];
+        MPI_Recv(&data, 3, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        double area = 0.0;
+        for(int i = 0; i < data[1]; i++){
+            double x_i = (data[0] + (i * data[2]));
+            area += data[2] * (log( x_i ) * pow( x_i , 2.0));
         }
-        int vals[2];
-        vals[0] = circle_points;
-        vals[1] = square_points;
-        MPI_Send(&vals, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
+        MPI_Send(&area, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 
     }
     MPI_Finalize();
